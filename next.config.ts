dbfbf64f88f51ea9b281e40ffbce99407ea8357e
Somesh Bhatnagar <someshbhatnagar544@gmail.com>
2026-04-09 23:52:12 +0530

    Update Config File

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   turbopack: {
     root: __dirname,
   },
